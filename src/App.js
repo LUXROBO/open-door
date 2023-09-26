6c6e87b3d61bb5e226fda4d0daf2208e40e0d4a6
tony <johnkim994@naver.com>
2023-09-26 11:27:43 +0900

    시연용 페이지 작업중

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleClassName = () => {
+    setIsActive((prevIsActive) => !prevIsActive);
+  };
+
+  useEffect(() => {
+    const socket = new WebSocket(
+      "wss://dev-notification.luxrobo.com/notification/subscriptions",
+    );
+
+    socket.addEventListener("open", (event) => {
+      console.log("소켓 연결됨");
+    });
+
+    socket.addEventListener("close", (event) => {
+      console.log("소켓 연결 해제됨");
+    });
+
+    socket.addEventListener("message", (event) => {
+      console.log("서버로부터의 메시지:", event.data);
+      toggleClassName();
+    });
+
+    return () => {
+      socket.close();
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" onClick={toggleClassName}>
+      header
+      <div className={isActive ? "door active" : "door"} id="door1">
+        <div className="grid-item-interior">
+          <img
+            className="inner-background"
+            src="/assets/bg.jpg"
+            alt="background"
+          />
+        </div>
+        <div className="left"></div>
+        <div className="right"></div>
+      </div>
     </div>
   );
 }
